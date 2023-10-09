f91f9f304652b5eb5bb21050fa44132d0de1de30
SAGAR KUMAR <126503955+xsea29@users.noreply.github.com>
2023-10-09 12:29:29 +0530

    Update script.js
    New feature added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Code starts here...
 
 let player = 1;
-let resultArr = ['', '', '', '', '', '', '', '' ,''];
+let resultArr = ["", "", "", "", "", "", "", "", ""];
 let playing = false;
 
 const winningCombination = [
@@ -16,15 +16,17 @@ const winningCombination = [
   [2, 4, 6],
 ];
 
+const newGame = document.querySelector(".new-game");
+
 //Initialise the GAME
-const initializeGame = function(){
-   document.querySelectorAll(".cell").forEach((cell) => (cell.textContent = ""));
-  resultArr = ['', '', '', '', '', '', '', '' ,''];
+const initializeGame = function () {
+  document.querySelectorAll(".cell").forEach((cell) => (cell.textContent = ""));
+  resultArr = ["", "", "", "", "", "", "", "", ""];
   playing = true;
   player = 1;
   document.querySelector(".winner--1").classList.add("hidden");
   document.querySelector(".winner--2").classList.add("hidden");
-}
+};
 
 const tictactoe = function (num) {
   if (playing) {
@@ -41,17 +43,32 @@ const tictactoe = function (num) {
       //Check for Player 1's win
       if (checkForWin(1)) {
         console.log("🎉Player 1 wins");
-        document.querySelector(".winner--1").classList.remove("hidden");
+        // document.querySelector(".winner--1").classList.remove("hidden");
+        document.querySelector(".draw").style.zIndex = "2";
+        document.querySelector(".draw").style.opacity = "100%";
+        document.querySelector(".draw--0").textContent = "👑PLAYER 1 WINS";
+        newGame.style.zIndex = "2";
+        newGame.style.opacity = "100%";
         playing = false;
       }
       //Check for Player 2's win
       else if (checkForWin(0)) {
         console.log("🎉Player 2 wins");
-        document.querySelector(".winner--2").classList.remove("hidden");
+        // document.querySelector(".winner--2").classList.remove("hidden");
+        document.querySelector(".draw").style.zIndex = "2";
+        document.querySelector(".draw").style.opacity = "100%";
+        document.querySelector(".draw--0").textContent = "👑PLAYER 2 WINS";
+        newGame.style.zIndex = "2";
+        newGame.style.opacity = "100%";
         playing = false;
-      } else if(checkForDraw(resultArr)){
-        console.log("🤝DRAW!!!");
-      }else {
+      } else if (checkForDraw(resultArr)) {
+        // console.log("🤝DRAW!!!");
+        document.querySelector(".draw").style.zIndex = "2";
+        document.querySelector(".draw").style.opacity = "100%";
+        document.querySelector(".draw--0").textContent = "🤝DRAW";
+        newGame.style.zIndex = "2";
+        newGame.style.opacity = "100%";
+      } else {
         player = player === 1 ? 0 : 1;
         //Change the turn
       }
@@ -85,14 +102,21 @@ const checkForDraw = function (board) {
   return board.every((cell) => cell !== "");
 };
 
-
-//Reload button
+//Reload buttion
 document.querySelector(".reload").addEventListener("click", initializeGame);
 
 // setting up the start button
 document.querySelector(".start--0").addEventListener("click", function () {
   console.log("START THE GAME");
-   playing = true;
+  playing = true;
   initializeGame();
 });
-  
+
+//New-Game Button
+newGame.addEventListener("click", function () {
+  newGame.style.zIndex = "-1";
+  newGame.style.opacity = "0%";
+  document.querySelector(".draw").style.zIndex = "-1";
+  document.querySelector(".draw").style.opacity = "0%";
+  initializeGame();
+});
